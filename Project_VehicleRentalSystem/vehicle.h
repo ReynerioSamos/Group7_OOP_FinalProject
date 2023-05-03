c171c068d5586af2385423a488da5bab7853ecfa
Reynerio Samos <Reynerio.Samos@gmail.com>
2023-05-03 14:53:01 -0600

    modified:   vehicle.h

--- Project_VehicleRentalSystem/vehicle.h
+++ Project_VehicleRentalSystem/vehicle.h
@@ -20,17 +20,43 @@ public:
     void setModel(QString model);
     void setYear (int x);
     void setDesc (QString desc);
-    void setNumOfPassengers(in)
+    void setNumOfPassengers(int passengers);
+    void setTransmissionID(int transid);
+    void setTransmission (QString trans);
+    void setFuelTypeID(int fuelid);
+    void setFuelType(QString fuel);
+    void setDrivetrainID(int dtrainid);
+    void setDrivetrain(QString dtrainid);
+    void setAvailability(bool status);
+    void setLastServiced(QDate lastserv);
+
+    //get Functions
+    int getVehicleID();
+    int getTotalMiles;
+    QString getMake();
+    QString getModel();
+    int getYear();
+    QString getDescription();
+    int getNumOfPassengers();
+    int getTransmissionID();
+    int getFuelTypeID();
+    int getDrivetrainID();
+    QString getTransmission();
+    QString getFuelType();
+    QString getDrivetrain;
+    bool getAvailability;
+    QDate getLastServiced;
 
 private:
     int vehicleID;
     int totalMiles;
-    QString Make, Model;
+    QString make, model;
     int year;
     //used to store additional info, like if car is push to start, or has manual windows
     QString description;
     int num_of_passengers;
     int transmissionID, fuelTypeID, drivetrainID;
+    QString transmission, fuelType, drivetrain;
     bool availability;
     QDate lastServiced;
 
