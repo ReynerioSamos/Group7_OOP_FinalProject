b753be55e2d7191603dea159c0283e78657ff7df
Reynerio Samos <Reynerio.Samos@gmail.com>
2023-05-15 12:04:31 -0600

    finish database.h & implement some query functions

--- Project_VehicleRentalSystem/database.cpp
+++ Project_VehicleRentalSystem/database.cpp
@@ -1,12 +1,157 @@
 #include "database.h"
+#include <QtSql>
 
 Database::Database(QObject *parent)
     : QObject{parent}
 {
+   db = QSqlDatabase::addDatabase("QMYSQL");
+   db.setHostName ("127.0.0.1");
+   db.setUserName ("root");
+   db.setPassword ("");
+   db.setDatabaseName ("vehicle_rental_service_db");
+
+   if(db.open())
+       qDebug() <<"Successful Connection to db.";
+   else if (!db.open())
+       qDebug() <<"Failed to connect to database.";
 
 }
 
 Database::~Database()
 {
+    closeDatabase();
+}
+
+bool Database::openDatabase()
+{
+    if (!db.open()){
+        qDebug() << "Failed to connect to database: " << db.lastError().text();
+        return false;
+    }
+
+    qDebug() << "Connected to database";
+    return true;
 
 }
+
+void Database::closeDatabase()
+{
+    db.close();
+    qDebug() << "Disconnected from database";
+}
+
+bool Database::insertCustomer(Customer& cust){
+
+    QSqlQuery query(db);
+
+    query.prepare("INSERT INTO customers (phoneNumber, email, dateAdded) "
+                  "VALUES (:phoneNumber, :email, :dateAdded);");
+
+    query.bindValue(":phoneNumber", cust.getCustomerPhoneNumber());
+    query.bindValue(":email", cust.getEmail());
+    query.bindValue(":dateAdded", cust.getDate_added());
+
+
+    if(!query.exec()){
+        qDebug() << "Failed to insert Customer: " << query.lastError().text();
+        return false;
+    }
+
+    qDebug() << "Customer inserted successfully";
+    return true;
+}
+
+bool Database::insertCompany(Company& comp, int custID){
+
+    QSqlQuery query(db);
+
+    query.prepare("INSERT INTO companies (CustomerID, companyName, dateFounded) "
+                  "VALUES (:CustomerID, :companyName, :dateFounded);");
+
+    query.bindValue(":CustomerID", custID);
+    query.bindValue(":companyName", comp.getCompanyName());
+    query.bindValue(":dateFounded", comp.getDateFounded());
+
+
+    if(!query.exec()){
+        qDebug() << "Failed to insert Company: " << query.lastError().text();
+        return false;
+    }
+
+    qDebug() << "Company inserted successfully";
+    return true;
+
+}
+
+bool Database::insertIndividual(individual& indiv, int custID)
+{
+
+    QSqlQuery query(db);
+
+    query.prepare("INSERT INTO individuals (CustomerID, firstName, lastName, DoB) "
+                  "VALUES (:CustomerID, :firstName, :lastName, :DoB);");
+
+    query.bindValue(":CustomerID", custID);
+    query.bindValue(":firstName", indiv.getFirstName());
+    query.bindValue(":lastName", indiv.getLastName());
+    query.bindValue(":DoB", indiv.getDob());
+
+
+    if(!query.exec()){
+        qDebug() << "Failed to insert Individual: " << query.lastError().text();
+        return false;
+    }
+
+    qDebug() << "Individual inserted successfully";
+    return true;
+
+}
+
+
+bool Database::insertPerson (Person& pers)
+{
+    QSqlQuery query(db);
+
+    query.prepare("INSERT INTO persons (firstName, lastName, DoB, phoneNumber, email) "
+                  "VALUES (:firstName, :lastName, :DoB, :phoneNumber, :email);");
+
+    query.bindValue(":firstName", pers.getFirstName());
+    query.bindValue(":lastName", pers.getLastName());
+    query.bindValue(":DoB", pers.getDoB());
+    query.bindValue(":phoneNumber", pers.getPhoneNum());
+    query.bindValue(":email", pers.getEmail());
+
+
+    if(!query.exec()){
+        qDebug() << "Failed to insert Person: " << query.lastError().text();
+        return false;
+    }
+
+    qDebug() << "Person inserted successfully";
+    return true;
+}
+
+bool Database::insertEmployee (Employee& emp, int persID, QString user, QString pass)
+{
+    QSqlQuery query(db);
+
+    query.prepare("INSERT INTO employees (PersonID, PositionID, salary, dateEmployed, username, password) "
+                  "VALUES (:PersonID, :PositionID, :salary, :dateEmployed, :username, :password);");
+
+    query.bindValue(":PersonID", persID);
+    query.bindValue(":PositionID", emp.getPositionID());
+    query.bindValue(":salary", emp.getSalary());
+    query.bindValue(":dateEmployed", emp.getDateEmployed());
+    query.bindValue("username", user);
+    query.bindValue(":password", pass);
+
+    if(!query.exec()){
+        qDebug() << "Failed to insert Employee: " << query.lastError().text();
+        return false;
+    }
+
+    qDebug() << "Employee inserted successfully";
+    return true;
+}
+
+
--- Project_VehicleRentalSystem/database.h
+++ Project_VehicleRentalSystem/database.h
@@ -28,10 +28,6 @@
 #include "technician.h"
 
 
-#include "stringformatters.h"
-
-
-
 class Database : public QObject
 {
     Q_OBJECT
@@ -44,26 +40,56 @@ public:
     void closeDatabase();
 
     // Insert Queries used to add into database
-    bool insertCustomer(const Customer& cust);
-    bool insertCompany (const Company& comp);
-    bool insertIndividual (const individual& indiv);
+    bool insertCustomer(Customer& cust);
+    bool insertCompany (Company& comp, int custID);
+    bool insertIndividual (individual& indiv, int custID);
 
-    bool insertPerson (const Person& pers);
-    bool insertEmployee (const Employee& emp);
-    bool insertDriver (const Driver& drvr);
-    bool insertTechnician (const Technician& tech );
+    bool insertPerson (Person& pers);
+    bool insertEmployee (Employee& emp, int persID, QString user, QString pass);
+    bool insertDriver (Driver& drvr, int persID);
+    bool insertTechnician (Technician& tech, int persID );
 
-    bool insertVehicle (const Vehicle& vehicleobj);
-    bool insertCar (const Car& carobj);
-    bool insertTruck (const Truck& truckobj);
-    bool insertBus (const Bus& busobj);
-    bool insertMotorbike (const Motorbike& bike);
+    bool insertVehicle (Vehicle& vehicleobj);
+    bool insertCar (Car& carobj, int vehicleID);
+    bool insertTruck (Truck& truckobj, int vehicleID);
+    bool insertBus (Bus& busobj, int vehicleID);
+    bool insertMotorbike (Motorbike& bike, int vehicleID);
 
     // Update Queries used to update existing elements in database
+    bool updateCustomer (Customer& cust);
+    bool updateCompany (Company& comp);
+    bool updateIndividual (individual& indiv);
+
+    bool updatePerson (Person& pers);
+    bool updateEmployee (Employee& emp);
+    bool updateDriver (Driver& drvr);
+    bool updateTechnician (Technician& tech );
+
+    bool updateVehicle (Vehicle& vehicleobj);
+    bool updateCar (Car& carobj);
+    bool updateTruck (Truck& truckobj);
+    bool updateBus (Bus& busobj);
+    bool updateMotorbike (Motorbike& bike);
 
     // Delete queries used to remove existing elements in database
+    // better to just pass the ID as it only uses the ID to make the deletion, no sense in passing and keeping an object
+    bool deleteCustomer (int custID);
+    bool deleteCompany (int compID);
+    bool deleteIndividual (int indivID);
 
+    bool deletePerson (int persID);
+    bool deleteEmployee (int empID);
+    bool deleteDriver (int drvrID);
+    bool deleteTechnician (int techID);
 
+    bool deleteVehicle (int vehicleID);
+    bool deleteCar (int carID);
+    bool deleteTruck (int truckID);
+    bool deleteBus (int busobjID);
+    bool deleteMotorbike (int bikeID);
+
+private:
+    QSqlDatabase db;
 
 signals:
 
